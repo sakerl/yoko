df9a197dcb01a9b269e0041e32009cc614bd7923
Neil Richards <7203556+ngmr@users.noreply.github.com>
2024-10-16 13:01:07 +0100

    style: improve imports

--- yoko-core/src/main/java/org/apache/yoko/orb/CORBA/NotImplementedError.java
+++ yoko-core/src/main/java/org/apache/yoko/orb/CORBA/NotImplementedError.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 IBM Corporation and others.
+ * Copyright 2024 IBM Corporation and others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
  */
 package org.apache.yoko.orb.CORBA;
 
-final public class NotImplementedError extends Error {
+public final class NotImplementedError extends Error {
     public NotImplementedError() {
         super("Not implemented yet");
     }
