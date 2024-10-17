7de9552109ae573dd977ca40b8048f89be46df6a
Neil Richards <7203556+ngmr@users.noreply.github.com>
2024-10-17 17:22:28 +0100

    style: improve imports

--- yoko-core/src/main/java/org/apache/yoko/orb/IMR/ArgListHelper.java
+++ yoko-core/src/main/java/org/apache/yoko/orb/IMR/ArgListHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 IBM Corporation and others.
+ * Copyright 2024 IBM Corporation and others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,17 @@
  */
 package org.apache.yoko.orb.IMR;
 
-import org.apache.yoko.util.MinorCodes;
+import static org.apache.yoko.util.MinorCodes.MinorTypeMismatch;
+import static org.apache.yoko.util.MinorCodes.describeBadOperation;
+import static org.omg.CORBA.CompletionStatus.COMPLETED_NO;
+
+import org.omg.CORBA.Any;
+import org.omg.CORBA.BAD_OPERATION;
+import org.omg.CORBA.ORB;
+import org.omg.CORBA.StringSeqHelper;
+import org.omg.CORBA.TypeCode;
+import org.omg.CORBA.portable.InputStream;
+import org.omg.CORBA.portable.OutputStream;
 
 //
 // IDL:orb.yoko.apache.org/IMR/ArgList:1.0
@@ -25,34 +35,33 @@ import org.apache.yoko.util.MinorCodes;
 final public class ArgListHelper
 {
     public static void
-    insert(org.omg.CORBA.Any any, String[] val)
+    insert(Any any, String[] val)
     {
-        org.omg.CORBA.portable.OutputStream out = any.create_output_stream();
+        OutputStream out = any.create_output_stream();
         write(out, val);
         any.read_value(out.create_input_stream(), type());
     }
 
     public static String[]
-    extract(org.omg.CORBA.Any any)
+    extract(Any any)
     {
         if(any.type().equivalent(type()))
             return read(any.create_input_stream());
         else
-            throw new org.omg.CORBA.BAD_OPERATION(
-                MinorCodes
-                        .describeBadOperation(MinorCodes.MinorTypeMismatch),
-                MinorCodes.MinorTypeMismatch, org.omg.CORBA.CompletionStatus.COMPLETED_NO);
+            throw new BAD_OPERATION(
+                describeBadOperation(MinorTypeMismatch),
+                MinorTypeMismatch, COMPLETED_NO);
     }
 
-    private static org.omg.CORBA.TypeCode typeCode_;
+    private static TypeCode typeCode_;
 
-    public static org.omg.CORBA.TypeCode
+    public static TypeCode
     type()
     {
         if(typeCode_ == null)
         {
-            org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init();
-            typeCode_ = orb.create_alias_tc(id(), "ArgList", org.omg.CORBA.StringSeqHelper.type());
+            ORB orb = ORB.init();
+            typeCode_ = orb.create_alias_tc(id(), "ArgList", StringSeqHelper.type());
         }
 
         return typeCode_;
@@ -65,16 +74,16 @@ final public class ArgListHelper
     }
 
     public static String[]
-    read(org.omg.CORBA.portable.InputStream in)
+    read(InputStream in)
     {
         String[] _ob_v;
-        _ob_v = org.omg.CORBA.StringSeqHelper.read(in);
+        _ob_v = StringSeqHelper.read(in);
         return _ob_v;
     }
 
     public static void
-    write(org.omg.CORBA.portable.OutputStream out, String[] val)
+    write(OutputStream out, String[] val)
     {
-        org.omg.CORBA.StringSeqHelper.write(out, val);
+        StringSeqHelper.write(out, val);
     }
 }
