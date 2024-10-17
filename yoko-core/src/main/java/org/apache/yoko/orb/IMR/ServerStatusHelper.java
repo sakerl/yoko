f211fc89cb5670f93a4cc9a54fae3429452aeada
Neil Richards <7203556+ngmr@users.noreply.github.com>
2024-10-17 17:28:08 +0100

    style: improve imports

--- yoko-core/src/main/java/org/apache/yoko/orb/IMR/ServerStatusHelper.java
+++ yoko-core/src/main/java/org/apache/yoko/orb/IMR/ServerStatusHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 IBM Corporation and others.
+ * Copyright 2024 IBM Corporation and others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,41 +17,49 @@
  */
 package org.apache.yoko.orb.IMR;
 
-import org.apache.yoko.util.MinorCodes;
+import static org.apache.yoko.util.MinorCodes.MinorTypeMismatch;
+import static org.apache.yoko.util.MinorCodes.describeBadOperation;
+import static org.omg.CORBA.CompletionStatus.COMPLETED_NO;
+
+import org.omg.CORBA.Any;
+import org.omg.CORBA.BAD_OPERATION;
+import org.omg.CORBA.ORB;
+import org.omg.CORBA.TypeCode;
+import org.omg.CORBA.portable.InputStream;
+import org.omg.CORBA.portable.OutputStream;
 
 //
 // IDL:orb.yoko.apache.org/IMR/ServerStatus:1.0
 //
-final public class ServerStatusHelper
+public final class ServerStatusHelper
 {
     public static void
-    insert(org.omg.CORBA.Any any, ServerStatus val)
+    insert(Any any, ServerStatus val)
     {
-        org.omg.CORBA.portable.OutputStream out = any.create_output_stream();
+        OutputStream out = any.create_output_stream();
         write(out, val);
         any.read_value(out.create_input_stream(), type());
     }
 
     public static ServerStatus
-    extract(org.omg.CORBA.Any any)
+    extract(Any any)
     {
         if(any.type().equivalent(type()))
             return read(any.create_input_stream());
         else
-            throw new org.omg.CORBA.BAD_OPERATION(
-                MinorCodes
-                        .describeBadOperation(MinorCodes.MinorTypeMismatch),
-                MinorCodes.MinorTypeMismatch, org.omg.CORBA.CompletionStatus.COMPLETED_NO);
+            throw new BAD_OPERATION(
+                describeBadOperation(MinorTypeMismatch),
+                MinorTypeMismatch, COMPLETED_NO);
     }
 
-    private static org.omg.CORBA.TypeCode typeCode_;
+    private static TypeCode typeCode_;
 
-    public static org.omg.CORBA.TypeCode
+    public static TypeCode
     type()
     {
         if(typeCode_ == null)
         {
-            org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init();
+            ORB orb = ORB.init();
             String[] members = new String[5];
             members[0] = "FORKED";
             members[1] = "STARTING";
@@ -71,7 +79,7 @@ final public class ServerStatusHelper
     }
 
     public static ServerStatus
-    read(org.omg.CORBA.portable.InputStream in)
+    read(InputStream in)
     {
         ServerStatus _ob_v;
         _ob_v = ServerStatus.from_int(in.read_ulong());
@@ -79,7 +87,7 @@ final public class ServerStatusHelper
     }
 
     public static void
-    write(org.omg.CORBA.portable.OutputStream out, ServerStatus val)
+    write(OutputStream out, ServerStatus val)
     {
         out.write_ulong(val.value());
     }
