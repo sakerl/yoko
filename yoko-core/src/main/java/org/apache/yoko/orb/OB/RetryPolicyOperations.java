a55f13df025e73f4311c1390c45532f3c0acb288
Neil Richards <7203556+ngmr@users.noreply.github.com>
2024-11-14 12:19:22 +0000

    style: improve imports

--- yoko-core/src/main/java/org/apache/yoko/orb/OB/RetryPolicyOperations.java
+++ yoko-core/src/main/java/org/apache/yoko/orb/OB/RetryPolicyOperations.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 IBM Corporation and others.
+ * Copyright 2024 IBM Corporation and others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,8 @@
  */
 package org.apache.yoko.orb.OB;
 
+import org.omg.CORBA.PolicyOperations;
+
 //
 // IDL:orb.yoko.apache.org/OB/RetryPolicy:1.0
 //
@@ -28,7 +30,7 @@ package org.apache.yoko.orb.OB;
  *
  **/
 
-public interface RetryPolicyOperations extends org.omg.CORBA.PolicyOperations
+public interface RetryPolicyOperations extends PolicyOperations
 {
     //
     // IDL:orb.yoko.apache.org/OB/RetryPolicy/retry_mode:1.0
