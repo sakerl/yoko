c9457d96d2bb12bb3d9b00c4ee74f5916afa76f7
Neil Richards <7203556+ngmr@users.noreply.github.com>
2024-11-14 12:14:47 +0000

    style: improve imports

--- yoko-core/src/main/java/org/apache/yoko/orb/OB/InterceptorPolicy_impl.java
+++ yoko-core/src/main/java/org/apache/yoko/orb/OB/InterceptorPolicy_impl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 IBM Corporation and others.
+ * Copyright 2024 IBM Corporation and others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,10 +17,10 @@
  */
 package org.apache.yoko.orb.OB;
 
-import org.apache.yoko.orb.OB.INTERCEPTOR_POLICY_ID;
-import org.apache.yoko.orb.OB.InterceptorPolicy;
+import org.omg.CORBA.LocalObject;
+import org.omg.CORBA.Policy;
 
-final public class InterceptorPolicy_impl extends org.omg.CORBA.LocalObject
+public final class InterceptorPolicy_impl extends LocalObject
         implements InterceptorPolicy {
     private boolean value_;
 
@@ -36,7 +36,7 @@ final public class InterceptorPolicy_impl extends org.omg.CORBA.LocalObject
         return INTERCEPTOR_POLICY_ID.value;
     }
 
-    public org.omg.CORBA.Policy copy() {
+    public Policy copy() {
         return this;
     }
 
