38e8d80d6ed096d26867d4afc6cd6bff6dce27b2
Neil Richards <7203556+ngmr@users.noreply.github.com>
2025-01-29 10:24:44 +0000

    style: improve imports

--- yoko-core/src/main/java/org/apache/yoko/orb/OBPortableInterceptor/PersistentORTHelper.java
+++ yoko-core/src/main/java/org/apache/yoko/orb/OBPortableInterceptor/PersistentORTHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 IBM Corporation and others.
+ * Copyright 2025 IBM Corporation and others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,6 +18,28 @@
 package org.apache.yoko.orb.OBPortableInterceptor;
 
 import org.apache.yoko.util.MinorCodes;
+import org.omg.CORBA.Any;
+import org.omg.CORBA.BAD_OPERATION;
+import org.omg.CORBA.BAD_PARAM;
+import org.omg.CORBA.ORB;
+import org.omg.CORBA.PRIVATE_MEMBER;
+import org.omg.CORBA.TypeCode;
+import org.omg.CORBA.VM_NONE;
+import org.omg.CORBA.ValueMember;
+import org.omg.CORBA.portable.InputStream;
+import org.omg.CORBA.portable.OutputStream;
+import org.omg.IOP.IORHelper;
+import org.omg.PortableInterceptor.AdapterNameHelper;
+import org.omg.PortableInterceptor.ORBIdHelper;
+import org.omg.PortableInterceptor.ServerIdHelper;
+
+import java.io.Serializable;
+
+import static org.apache.yoko.util.MinorCodes.MinorIncompatibleObjectType;
+import static org.apache.yoko.util.MinorCodes.MinorTypeMismatch;
+import static org.apache.yoko.util.MinorCodes.describeBadOperation;
+import static org.apache.yoko.util.MinorCodes.describeBadParam;
+import static org.omg.CORBA.CompletionStatus.COMPLETED_NO;
 
 //
 // IDL:orb.yoko.apache.org/OBPortableInterceptor/PersistentORT:1.0
@@ -25,59 +47,58 @@ import org.apache.yoko.util.MinorCodes;
 final public class PersistentORTHelper
 {
     public static void
-    insert(org.omg.CORBA.Any any, PersistentORT val)
+    insert(Any any, PersistentORT val)
     {
         any.insert_Value(val, type());
     }
 
     public static PersistentORT
-    extract(org.omg.CORBA.Any any)
+    extract(Any any)
     {
         if(any.type().equivalent(type()))
         {
-            java.io.Serializable _ob_v = any.extract_Value();
+            Serializable _ob_v = any.extract_Value();
             if(_ob_v == null || _ob_v instanceof PersistentORT)
                 return (PersistentORT)_ob_v;
         }
 
 
-        throw new org.omg.CORBA.BAD_OPERATION(
-            MinorCodes
-                    .describeBadOperation(MinorCodes.MinorTypeMismatch),
-            MinorCodes.MinorTypeMismatch, org.omg.CORBA.CompletionStatus.COMPLETED_NO);
+        throw new BAD_OPERATION(
+            describeBadOperation(MinorTypeMismatch),
+            MinorTypeMismatch, COMPLETED_NO);
     }
 
-    private static org.omg.CORBA.TypeCode typeCode_;
+    private static TypeCode typeCode_;
 
-    public static org.omg.CORBA.TypeCode
+    public static TypeCode
     type()
     {
         if(typeCode_ == null)
         {
-            org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init();
-            org.omg.CORBA.ValueMember[] members = new org.omg.CORBA.ValueMember[4];
+            ORB orb = ORB.init();
+            ValueMember[] members = new ValueMember[4];
 
-            members[0] = new org.omg.CORBA.ValueMember();
+            members[0] = new ValueMember();
             members[0].name = "the_server_id";
-            members[0].type = org.omg.PortableInterceptor.ServerIdHelper.type();
-            members[0].access = org.omg.CORBA.PRIVATE_MEMBER.value;
+            members[0].type = ServerIdHelper.type();
+            members[0].access = PRIVATE_MEMBER.value;
 
-            members[1] = new org.omg.CORBA.ValueMember();
+            members[1] = new ValueMember();
             members[1].name = "the_orb_id";
-            members[1].type = org.omg.PortableInterceptor.ORBIdHelper.type();
-            members[1].access = org.omg.CORBA.PRIVATE_MEMBER.value;
+            members[1].type = ORBIdHelper.type();
+            members[1].access = PRIVATE_MEMBER.value;
 
-            members[2] = new org.omg.CORBA.ValueMember();
+            members[2] = new ValueMember();
             members[2].name = "the_adapter_name";
-            members[2].type = org.omg.PortableInterceptor.AdapterNameHelper.type();
-            members[2].access = org.omg.CORBA.PRIVATE_MEMBER.value;
+            members[2].type = AdapterNameHelper.type();
+            members[2].access = PRIVATE_MEMBER.value;
 
-            members[3] = new org.omg.CORBA.ValueMember();
+            members[3] = new ValueMember();
             members[3].name = "the_ior_template";
-            members[3].type = org.omg.IOP.IORHelper.type();
-            members[3].access = org.omg.CORBA.PRIVATE_MEMBER.value;
+            members[3].type = IORHelper.type();
+            members[3].access = PRIVATE_MEMBER.value;
 
-            typeCode_ = orb.create_value_tc(id(), "PersistentORT", org.omg.CORBA.VM_NONE.value, null, members);
+            typeCode_ = orb.create_value_tc(id(), "PersistentORT", VM_NONE.value, null, members);
         }
 
         return typeCode_;
@@ -90,25 +111,23 @@ final public class PersistentORTHelper
     }
 
     public static PersistentORT
-    read(org.omg.CORBA.portable.InputStream in)
+    read(InputStream in)
     {
         if(!(in instanceof org.omg.CORBA_2_3.portable.InputStream)) {
-            throw new org.omg.CORBA.BAD_PARAM(MinorCodes
-                .describeBadParam(MinorCodes.MinorIncompatibleObjectType),
-                MinorCodes.MinorIncompatibleObjectType,
-                org.omg.CORBA.CompletionStatus.COMPLETED_NO);
+            throw new BAD_PARAM(describeBadParam(MinorIncompatibleObjectType),
+                MinorIncompatibleObjectType,
+                COMPLETED_NO);
         }
         return (PersistentORT)((org.omg.CORBA_2_3.portable.InputStream)in).read_value(id());
     }
 
     public static void
-    write(org.omg.CORBA.portable.OutputStream out, PersistentORT val)
+    write(OutputStream out, PersistentORT val)
     {
         if(!(out instanceof org.omg.CORBA_2_3.portable.OutputStream)) {
-            throw new org.omg.CORBA.BAD_PARAM(MinorCodes
-                .describeBadParam(MinorCodes.MinorIncompatibleObjectType),
-                MinorCodes.MinorIncompatibleObjectType,
-                org.omg.CORBA.CompletionStatus.COMPLETED_NO);
+            throw new BAD_PARAM(describeBadParam(MinorIncompatibleObjectType),
+                MinorIncompatibleObjectType,
+                COMPLETED_NO);
         }
         ((org.omg.CORBA_2_3.portable.OutputStream)out).write_value(val, id());
     }
