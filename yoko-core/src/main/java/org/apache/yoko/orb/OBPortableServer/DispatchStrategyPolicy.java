0a92b042d253e94c54e79655b7391cfa2ff6e918
Neil Richards <7203556+ngmr@users.noreply.github.com>
2025-02-27 09:57:33 +0000

    style: improve imports

--- yoko-core/src/main/java/org/apache/yoko/orb/OBPortableServer/DispatchStrategyPolicy.java
+++ yoko-core/src/main/java/org/apache/yoko/orb/OBPortableServer/DispatchStrategyPolicy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 IBM Corporation and others.
+ * Copyright 2025 IBM Corporation and others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,9 @@ package org.apache.yoko.orb.OBPortableServer;
 //
 // IDL:orb.yoko.apache.org/OBPortableServer/DispatchStrategyPolicy:1.0
 //
+
+import org.omg.CORBA.Policy;
+
 /**
  *
  * The dispatch strategy policy. This policy controls the method
@@ -30,6 +33,6 @@ package org.apache.yoko.orb.OBPortableServer;
  **/
 
 public interface DispatchStrategyPolicy extends DispatchStrategyPolicyOperations,
-                                                org.omg.CORBA.Policy
+                                                Policy
 {
 }
